df49a7f9e187e229cef9c437fb49a52dd75d5fab
Vitor Hugo Campos <vitorh45@gmail.com>
2014-04-18 10:10:28 -0300

    Debug false

--- eventex/settings.py
+++ eventex/settings.py
@@ -1,4 +1,5 @@
-import dj_database_url
+from decouple import config
+from dj_database_url import parse as db_url
 from unipath import Path
 BASE_DIR = Path(__file__).parent
 
@@ -7,14 +8,14 @@ BASE_DIR = Path(__file__).parent
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hhi(7zt*ro-6f_z=2^tu0_9$izrf40kycer^_s!ltef%gxb=p6'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -47,9 +48,9 @@ WSGI_APPLICATION = 'eventex.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + BASE_DIR.child('db.sqlite3')
-    )
+    'default': config(
+        'DATABASE_URL',        
+        default='sqlite:///' + BASE_DIR.child('db.sqlite3'), cast=db_url)
 }
 
 # Internationalization
